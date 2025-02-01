bd1c3c1716a11d8d91ea596e512718175a08995b
chalermsak-s <chalermsak_s@cmu.ac.th>
2025-02-01 16:53:33 +0700

    Task 8.4

--- test-function.js
+++ test-function.js
@@ -1,5 +1,13 @@
 function greet(name){
     return 'Hello ' + name
 }
+function sumScore(student){
+    let element = 0;
+    for (let index = 0; index < student.length; index++) {
+        element += student[index];
+    }
+    return element
+}
+const stdData = [10,20,30];
+console.log(greet('John')+"Score "+sumScore(stdData))
 
-console.log(greet('John'))
