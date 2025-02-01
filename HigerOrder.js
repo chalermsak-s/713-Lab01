818702f50807e39246d114c890a2f71789adaff0
chalermsak-s <chalermsak_s@cmu.ac.th>
2025-02-01 17:18:11 +0700

    Task 12.3

--- HigerOrder.js
+++ HigerOrder.js
@@ -1,3 +1,4 @@
 const n = [1, 2, 3, 4, 5];
-const even = n.filter((num) => num % 2 === 0);
-console.log(even);
+const even = n.filter((num) => num % 3 === 0);
+const square3 = even.map((num) => num * num * num);
+console.log(square3);
