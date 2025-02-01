730d3a4404d244877af566968463cae20ed76bb4
chalermsak-s <chalermsak_s@cmu.ac.th>
2025-02-01 17:12:11 +0700

    Task 11.4

--- json.js
+++ json.js
@@ -1,9 +1,26 @@
-const object = {
-    name: "John",
-    age: 30,
-    city: "New York"
-}
+const student1 = {
+    firstName: "Somchai",
+    lastName: "Sukjai",
+    age: 20,
+    gpa: 3.11
+};
 
-console.log(object.name)
-const jsonStr = JSON.stringify(object);
-console.log(jsonStr)
+const student2 = {
+    firstName: "Anan",
+    lastName: "Boonmee",
+    age: 22,
+    gpa: 1.8
+};
+
+const checkStudent = (std)=>{
+    if(std.gpa > 2){
+        return JSON.stringify(std);
+    }else{
+        return `คุณ ${std.firstName} พ้นสภาพ`;
+    }
+};
+
+console.log(checkStudent(student1));
+console.log(checkStudent(student2));
+
+
