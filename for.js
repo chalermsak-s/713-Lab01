62cb93285becb0fd025c01342458331573bafe0f
chalermsak-s <chalermsak_s@cmu.ac.th>
2025-02-01 16:03:13 +0700

    Task 7.5

--- for.js
+++ for.js
@@ -1,4 +1,8 @@
-let color = ['red', 'blue', 'green', 'yellow', 'orange', 'purple']
-for (let i = 0; i < color.length; i++) {
-  console.log(color[i])
-}
+const names = ["Somchai", "Anan", "Nattapong", "Boonmee", "Lek"];
+names.forEach(name => {
+    if (name.length > 6) {
+        console.log(`สวัสดีคุณ ${name}`);
+    } else {
+        console.log(`Hello ${name}`);
+    }
+});
