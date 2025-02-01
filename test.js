d33fcd7d2e39a32b89e74dccafb56663544fb155
chalermsak-s <chalermsak_s@cmu.ac.th>
2025-02-01 15:55:10 +0700

    Task 6.8

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
-let x = 6
-let y = 13
-
-if(x > 5 && y > 12){
-    console.log('ค่ามากจัง');
-}else{
-    console.log(x*y + x/y );
+let x =7
+if (x > 10) {
+  console.log('x is more than 10')
+} else if (x < 5){
+    console.log('x is less than 5')
+}
+else {
+  console.log('x is between 5 and 10')
 }
