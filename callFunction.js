0ebbcb000a8346a74de101c26647b250c8b7e9f3
chalermsak-s <chalermsak_s@cmu.ac.th>
2025-02-01 17:02:57 +0700

    Task 10.4

--- callFunction.js
+++ callFunction.js
@@ -1,5 +1,2 @@
-const multiply = (x, y) => { return x * y }
-console.log(multiply(5, 10))
-module.exports = {
-    multiply
-};
+const { multiply } = require('./function');
+console.log(multiply(2, 3));
---
+++ function.js
@@ -0,0 +1,5 @@
+const multiply = (x, y) => { return x * y }
+console.log(multiply(5, 10))
+module.exports = {
+    multiply
+};
